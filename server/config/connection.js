b221f31b18b318b870b8fad3a1d13b007ff96129
mojsun <m.khorashahi7@gmail.com>
2023-11-21 15:14:11 -0500

    changing connection info again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,36 +1,36 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/choice-shopping",
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   }
-// );
-
-// module.exports = mongoose.connection;
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
-// Replace <password> with the password for the Moji-admin user. Encode if necessary.
-const uri = process.env.MONGODB_URI;
-
-const client = new MongoClient(uri, {
-  serverApi: ServerApiVersion.v1,
-});
-
-async function run() {
-  try {
-    await client.connect();
-    console.log("Connected successfully to MongoDB Atlas!");
-
-    // Here you can add the operations you want to perform on the database.
-  } catch (err) {
-    console.error("Connection to MongoDB Atlas failed!", err);
-  } finally {
-    await client.close();
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/choice-shopping",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   }
-}
+);
 
-run().catch(console.dir);
+module.exports = mongoose.connection;
+// const { MongoClient, ServerApiVersion } = require("mongodb");
 
-module.exports = client; // Only if you need to export the client for use elsewhere
+// // Replace <password> with the password for the Moji-admin user. Encode if necessary.
+// const uri = process.env.MONGODB_URI;
+
+// const client = new MongoClient(uri, {
+//   serverApi: ServerApiVersion.v1,
+// });
+
+// async function run() {
+//   try {
+//     await client.connect();
+//     console.log("Connected successfully to MongoDB Atlas!");
+
+//     // Here you can add the operations you want to perform on the database.
+//   } catch (err) {
+//     console.error("Connection to MongoDB Atlas failed!", err);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// run().catch(console.dir);
+
+// module.exports = client; // Only if you need to export the client for use elsewhere
